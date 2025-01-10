926e99ea195f485ae232c4fe7f5f563c16e05926
jerin32 <nusratjerin632@gmail.com>
2025-01-10 16:53:59 +0600

    explore-views

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return 'welcome to laravel';
 });
+
+Route::get('/greeting', function () {
+    return view('greeting');
+});
+
+Route::get('/users', function () {
+    $users = ['John', 'Jane', 'Doe'];
+    return view('users', compact('users'));
+});
